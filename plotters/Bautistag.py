cc86d9bf0137f304890e8c6e58805882aea4eb87
bautista-gilardon <bautistagilardon@gmail.com>
2024-04-22 15:45:15 -0300

    Defino la base

---
+++ plotters/Bautistag.py
@@ -0,0 +1,13 @@
+import seaborn.objects as so
+from gapminder import gapminder
+
+def plot():
+    figura = (
+        so.Plot(
+        )
+    )
+    return dict(
+        descripcion="",
+        autor="Bautista Gilardon",
+        figura=figura,
+    )
