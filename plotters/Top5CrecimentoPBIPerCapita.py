101324d3b091173aa5f1351d58f8bc1cc08c0a76
bautista-gilardon <bautistagilardon@gmail.com>
2024-05-03 10:52:57 -0300

    Version Final

--- plotters/Top5CrecimentoPBIPerCapita.py
+++ plotters/Top5CrecimentoPBIPerCapita.py
@@ -1,6 +1,5 @@
 import seaborn.objects as so
 from gapminder import gapminder
-MayorCrecimiento = gapminder[gapminder['country'].isin(['Botswana','Singapore','Korea, Rep.','Taiwan','Equatorial Guinea'])]
 
 def plot():
     MayorCrecimiento = gapminder[gapminder['country'].isin(['Singapore',
@@ -13,13 +12,14 @@ def plot():
     figura = (
     so.Plot(MayorCrecimiento, x = 'year', y = 'gdpPercap')
     .add(so.Line(), color = 'Top 5 Paises')
-    .add(so.Line(color='gray'), so.Agg(), data=gapminder, x = 'year', y = 'gdpPercap')
+    .add(so.Line(color='gray', linestyle=(4, 3)), so.Agg(), data=gapminder, x = 'year', y = 'gdpPercap')
     .label(title='Top 5 paises con el mayor creciento periodo 1952-2007', y = 'PBI per Capita', x = 'Year')
     .show()
     )
     return dict(
         descripcion="""Comparacion entre los 5 paises con el mayor crecimiento en el periodo 1952-2007.
-        Estos paises fueron seleccionados segun el indice PBIPerCap2007 / PBIPerCap1952
+        Estos paises fueron seleccionados segun el indice:  PBIPerCap2007 / PBIPerCap1952.
+        En gris, y linea punteada, se puede apreciar el promedio mundial anual de PBI per capita.
         """,
         autor="Bautista Gilardon",
         figura=figura,
